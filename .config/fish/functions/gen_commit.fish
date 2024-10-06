841eccba46122e11e2c2deb74d64baf519481ba1
Omer Cohen <omercohen990@gmail.com>
2024-10-06 15:19:45 +0300

    update

--- .config/fish/functions/gen_commit.fish
+++ .config/fish/functions/gen_commit.fish
@@ -1,15 +1,15 @@
 function gen_commit
     # Generate commit message
-    set commit_msg (git diff | gen_commit_msg)
+    set commit_msg "$(git diff | gen_commit_msg)"
 
     # Display the generated commit message
-    echo "Generated commit message:"
-    echo "$commit_msg"
-
+    echo "Generated commit message:
+    "
+    echo "$commit_msg
+    "
 
     if read_confirm
         git commit -am "$commit_msg"
-        echo "Commit created with the message: $commit_msg"
     end
     echo "Commit aborted."
 end
