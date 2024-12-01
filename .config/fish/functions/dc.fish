4057c1a520a88a9ff7890484e98ef7afdc074c52
Omer Cohen <omercohen990@gmail.com>
2024-12-02 00:59:11 +0200

    update

--- .config/fish/functions/dc.fish
+++ .config/fish/functions/dc.fish
@@ -1,14 +1,14 @@
 set base_command "docker compose -f docker-compose.yml -f docker-compose.debug.yml"
 
 function dc
-  set command (echo -e "logs -f --tail 100\nup --force-recreate -d\nbuild\nbuild --no-cache\nup -d" | fzf --prompt="Select command: ")
+  set command (echo -e "logs -f --tail 100\nup --force-recreate -d\nbuild\nbuild --no-cache\nup -d" | fzf --prompt="Command: ")
 
   if test -z "$command"
     commandline --function repaint
     return
   end
 
-  set services (echo -e "all\n$(docker compose ps --services)" | fzf --prompt="Select services: " --multi)
+  set services (echo -e "all\n$(docker compose ps --services)" | fzf --prompt="Services: " --multi)
 
   if test -z "$services"
     commandline --function repaint
